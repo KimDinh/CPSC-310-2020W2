930dc3c0c440413fb7b9aababf0078c6d5069370
Kim Dinh <hoangkimidinh1608@gmail.com>
2021-02-01 00:55:05 +0700

    Checking existence of WHERE and OPTIONS in query

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -22,7 +22,28 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public performQuery(query: any): Promise<any[]> {
-        return Promise.reject("Not implemented.");
+        return new Promise<any[]>((resolve, reject) => {
+            // if query does not contain WHERE or query["WHERE"] is null or undefined
+            if (!("WHERE" in query) ||  query["WHERE"] === null || query["WHERE"] === undefined) {
+                return reject(new InsightError("Missing 'WHERE' in query"));
+            }
+            // if query does not contain OPTIONS or query["OPTIONS"] is null or undefined
+            if (!("OPTIONS" in query) || !query["OPTIONS"] === null || !query["OPTIONS"] === undefined) {
+                return reject(new InsightError(("Missing 'OPTIONS' in query")));
+            }
+            // if query contains keys other than WHERE and OPTIONS
+            if (Object.keys(query).length > 2) {
+                return reject(new InsightError("Redundant keys in query"));
+            }
+            // TODO: check if ids in query refer to the same and existing dataset
+            try {
+                // TODO: perform WHERE
+                // TODO: perform OPTIONS
+            } catch (e) {
+                // InsightError or ResultTooLargeError caught
+                reject(e);
+            }
+        });
     }
 
     public listDatasets(): Promise<InsightDataset[]> {
